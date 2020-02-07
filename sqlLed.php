c6c7116747181fd24202e73a339d689c4af50303
TotalEpicness <42422230+PhantomEpicness@users.noreply.github.com>
2020-02-07 09:22:46 -1000

    Update sqlLed.php

--- sqlLed.php
+++ sqlLed.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "root";
+$username = "user";
 $password = "password";
-$dbname = "students";
+$dbname = "school";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
